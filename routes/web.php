6a49d6f6eee13dc2b7d0463f32dcc6fc1ff626e4
josemauricioacev <122044542@upq.edu.mx>
2025-03-30 02:42:58 -0600

    Fix: ensure public folder with index.php is deployed

--- routes/web.php
+++ routes/web.php
@@ -129,13 +129,6 @@ Route::get('/cancelado', function () {
 
 
 
-use Illuminate\Support\Facades\DB;
-
-Route::get('/test-db', function () {
-    try {
-        DB::connection()->getPdo();
-        return '✅ Conexión exitosa a la base de datos.';
-    } catch (\Exception $e) {
-        return '❌ Error de conexión: ' . $e->getMessage();
-    }
+Route::get('/prueba', function () {
+    return "¡Laravel en Azure funciona!";
 });
